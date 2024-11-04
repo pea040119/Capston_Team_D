78afd40b45f75b378e0d75754456ccfce06df3b2
Jung <qwerty3078@naver.com>
2024-11-04 20:09:21 +0900

    Tutor list

--- frontend/src/pages/Student.jsx
+++ frontend/src/pages/Student.jsx
@@ -1,6 +1,42 @@
+import React, { useState, useEffect } from 'react';
 import Box from '../components/Box';
+import TutorItem from '../components/TutorItem';
+import TutorModal from '../components/TutorModal';
+import Button from '../components/Button';
+import Content from '../components/Content';
+import './Student.css';
+
 const Student = () => {
-  return <Box text={10} />;
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [tutors, setTutors] = useState([]);
+  const addTutor = (tutor) => {
+    setTutors([...tutors, tutor]);
+  };
+  return (
+    <>
+      <div className="TutorList">
+        <Box text={'과외 목록'} type={'gray'} />
+        {tutors.map((tutor, index) => (
+          <TutorItem
+            key={index}
+            tutorname={tutor.tutorname}
+            tutorsch={tutor.tutorsch}
+            tutorsub={tutor.tutorsub}
+          />
+        ))}
+        <Button
+          onClick={() => setIsModalOpen(true)}
+          className="add-tutor-button"
+          text={'등록'}
+          style={{ backgroundColor: '#40B3DE', color: 'white' }}
+        />
+      </div>
+
+      {isModalOpen && (
+        <TutorModal onClose={() => setIsModalOpen(false)} onSave={addTutor} />
+      )}
+    </>
+  );
 };
 
 export default Student;
