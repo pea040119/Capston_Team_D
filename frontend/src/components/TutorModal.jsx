94afde55a67a8ae0ba3f0709e3b23f1071934d40
romans0826 <ans7092@g.skku.edu>
2024-11-14 08:27:45 +0900

    student_home

--- frontend/src/components/TutorModal.jsx
+++ frontend/src/components/TutorModal.jsx
@@ -1,62 +1,36 @@
 import React, { useState } from 'react';
 import './TutorModal.css';
-
 const TutorModal = ({ onClose, onSave }) => {
   const [tutorname, setName] = useState('');
   const [tutorsch, setTime] = useState('');
   const [tutorsub, setSubject] = useState('');
-  const [reward, setreward] = useState('');
-
+  const [reward, setReward] = useState('');
   const handleSave = () => {
-    onSave({
-      tutorname,
-      tutorsub,
-      tutorsch,
-      reward,
-    });
+    onSave({ tutorname, tutorsch, tutorsub, reward: parseInt(reward) || 0 });
     onClose();
   };
-
-  // const handleSave = () => {
-  //   const newTutor = {
-  //     tutorname,
-  //     tutorsub,
-  //     tutorsch,
-  //     reward,
-  //     schedule: [],
-  //   };
-  //   onSave(newTutor);
-  //   onClose();
-  // };
-
-  // const isSaveDisabled = !tutorname || !tutorsch || !tutorsub || !reward;
-
-  // const handleSave = () => {
-  //   onSave({ tutorname, tutorsch, tutorsub, reward: parseInt(reward) || 0 });
-  //   onClose();
-  // };
-
   return (
     <div className="tutormodal">
       <div className="tutormodal-content">
-        <h2>선생님 추가</h2>
-        <label>이름</label>
+        <h2>과외 추가</h2>
+        <label>선생님 성함</label>
         <input value={tutorname} onChange={(e) => setName(e.target.value)} />
         <label>시간</label>
         <input value={tutorsch} onChange={(e) => setTime(e.target.value)} />
         <label>과목</label>
         <input value={tutorsub} onChange={(e) => setSubject(e.target.value)} />
-        <label>상점</label>
-        <input value={reward} onChange={(e) => setreward(e.target.value)} />
-
-        {/* <button onClick={handleSave} disabled={isSaveDisabled}>
-          저장
-        </button> */}
+        <div className="reward-output">
+          <label>상점</label>
+          <input
+            type="number"
+            value={reward}
+            onChange={(e) => setReward(e.target.value)}
+          />
+        </div>
         <button onClick={handleSave}>저장</button>
         <button onClick={onClose}>닫기</button>
       </div>
     </div>
   );
 };
-
 export default TutorModal;
--- frontend/src/pages/StudentHome.jsx
+++ frontend/src/pages/StudentHome.jsx
@@ -1,245 +1,21 @@
-// import React, { useState, useEffect } from 'react';
-// import Cbutton from '../components/Cbutton';
-// import './StudentHome.css';
-// import Header from '../components/Header';
-// import Box from '../components/Box';
-// import rarrow from '../img/rightarrow.png';
-// import larrow from '../img/leftarrow.png';
-// import logo from '../img/logo.png';
-// import TutorItem from '../components/TutorItem';
-// import TutorModal from '../components/TutorModal';
-// // import StudentModal from '../components/StudentModal';
-// // import won from '../img/won.png';
-// import Button from '../components/Button';
-// import Content from '../components/Content';
-// import MiniCalendar from '../components/MiniCalendar';
-// // import TutorItem from '../components/TutorItem';
-
-// const StudentHome = () => {
-//   const [isModalOpen, setIsModalOpen] = useState(false);
-//   const [students, setStudents] = useState([]);
-//   const [totalFee, setTotalFee] = useState(0);
-//   const [currentWeek, setCurrentWeek] = useState(new Date());
-//   const [today, setToday] = useState({ formattedDate: '', formattedDay: '' });
-
-//   const days = [
-//     '월요일',
-//     '화요일',
-//     '수요일',
-//     '목요일',
-//     '금요일',
-//     '토요일',
-//     '일요일',
-//   ];
-
-//   useEffect(() => {
-//     const date = new Date();
-//     const dayIndex = date.getDay();
-//     const dayName = days[(dayIndex + 6) % 7];
-//     const formattedDate = `${date.getDate()}일`;
-//     const formattedDay = `(${dayName[0]})`;
-//     setToday({ formattedDate, formattedDay });
-//   }, []);
-
-//   const addStudent = (student) => {
-//     const newSchedule = student.schedule.reduce((acc, sch) => {
-//       const key = sch.time;
-//       if (acc[key]) {
-//         acc[key].push(sch.day);
-//       } else {
-//         acc[key] = [sch.day];
-//       }
-//       return acc;
-//     }, {});
-
-//     const formattedSchedule = Object.entries(newSchedule).map(
-//       ([time, days]) => `${days.join(', ')} ${time}`
-//     );
-
-//     const updatedStudent = {
-//       ...student,
-//       formattedSchedule,
-//     };
-
-//     setStudents([...students, updatedStudent]);
-//     setTotalFee(totalFee + student.fee);
-//   };
-
-//   const calculateTopPosition = (time) => {
-//     const [hour, minute] = time.split(':').map(Number);
-//     const hourOffset = (hour - 9) * 20;
-//     const minuteOffset = (minute / 60) * 20;
-//     return hourOffset + minuteOffset;
-//   };
-
-//   const changeWeek = (direction) => {
-//     const newDate = new Date(currentWeek);
-//     newDate.setDate(currentWeek.getDate() + direction * 7);
-//     setCurrentWeek(newDate);
-//   };
-
-//   const getWeekOfMonth = (date) => {
-//     const firstDay = new Date(date.getFullYear(), date.getMonth(), 1).getDay();
-//     return Math.ceil((date.getDate() + firstDay) / 7);
-//   };
-
-//   return (
-//     <>
-//       <MiniCalendar />
-//       <img src={logo} alt="logo" className="logo" />
-//       <div className="header-container">
-//         <Header
-//           title={`${currentWeek.getMonth() + 1}월 ${getWeekOfMonth(
-//             currentWeek
-//           )}주차`}
-//           leftchild={
-//             <img
-//               src={larrow}
-//               alt="Left"
-//               className="header-image"
-//               onClick={() => changeWeek(-1)}
-//             />
-//           }
-//           rightchild={
-//             <img
-//               src={rarrow}
-//               alt="Right"
-//               className="header-image"
-//               onClick={() => changeWeek(1)}
-//             />
-//           }
-//         />
-//       </div>
-//       <div className="box-container">
-//         <Box text={'이번주 진도'} />
-//         <div className="week-progress"></div>
-//       </div>
-
-//       <div className="box-container">
-//         <div className="week-homework">
-//           <Box text={'이번주 숙제 '} />
-//         </div>
-//       </div>
-//       {/* <div className="box-container">
-//         <Box text={'이달의 수입'} />
-//         <div className="total-fee">
-//           <img src={won} alt="won" className="won" />
-//           <div>{totalFee.toLocaleString()}원</div>
-//         </div>
-//       </div> */}
-
-//       <div className="calendar-container">
-//         <div className="calendar">
-//           {days.map((day, index) => {
-//             const date = new Date(currentWeek);
-//             date.setDate(
-//               currentWeek.getDate() - ((date.getDay() + 6) % 7) + index
-//             );
-//             const formattedDate = `${date.getDate()}일`;
-//             const formattedDay = `(${day[0]})`;
-
-//             return (
-//               <div key={day} className="day-section">
-//                 <Cbutton
-//                   text={
-//                     <>
-//                       {formattedDate}
-//                       <span className="small-text">{formattedDay}</span>
-//                     </>
-//                   }
-//                   type={
-//                     today?.formattedDate === formattedDate
-//                       ? 'selected'
-//                       : 'default'
-//                   }
-//                   onClick={() => {}}
-//                 />
-//                 <div
-//                   className="day-content"
-//                   style={{ position: 'relative', marginTop: '10px' }}
-//                 >
-//                   {students.flatMap((student, studentIndex) =>
-//                     student.schedule
-//                       .filter((sch) => sch.day === day[0])
-//                       .map((sch) => (
-//                         <div
-//                           key={`${student.name}-${sch.time}`}
-//                           style={{
-//                             position: 'absolute',
-//                             top: `${calculateTopPosition(sch.time)}px`,
-//                           }}
-//                         >
-//                           <Content
-//                             time={sch.time}
-//                             name={`${student.name} ${student.subject}`}
-//                             index={studentIndex} // 색상 순환을 위해 studentIndex를 전달
-//                             onMove={() => {}}
-//                           />
-//                         </div>
-//                       ))
-//                   )}
-//                 </div>
-//               </div>
-//             );
-//           })}
-//         </div>
-//         <div className="StudentList">
-//           <Box text={'과외 목록 | 상점 '} type={'gray'} />
-//           {students.map((Tutor, index) => (
-//             <TutorItem
-//               key={index}
-//               tutorsub={tutor.tutorsub}
-//               tutorname={tutor.tutorname}
-//               tutorsch={tutor.tutorsch}
-//               reward={tutor.reward}
-//               // name={student.name}
-//               // sch={(student.formattedSchedule || []).join(', ')}
-//               // grade={student.grade}
-//               // sub={student.subject}
-//             />
-//           ))}
-//           <div className="ButtonContainer">
-//             <Button
-//               onClick={() => setIsModalOpen(true)}
-//               text={'등록'}
-//               style={{
-//                 backgroundColor: '#6ac665',
-//                 color: 'white',
-//                 left: 130,
-//               }}
-//             />
-//           </div>
-//         </div>
-//       </div>
-//       {isModalOpen && (
-//         <TutorModal onClose={() => setIsModalOpen(false)} onSave={addStudent} />
-//       )}
-//     </>
-//   );
-// };
-
-// export default StudentHome;
 import React, { useState, useEffect } from 'react';
 import Cbutton from '../components/Cbutton';
-import './StudentHome.css';
 import Header from '../components/Header';
 import Box from '../components/Box';
 import rarrow from '../img/rightarrow.png';
 import larrow from '../img/leftarrow.png';
 import logo from '../img/logo.png';
-import TutorItem from '../components/TutorItem';
-import TutorModal from '../components/TutorModal';
-// import StudentModal from '../components/StudentModal';
-// import won from '../img/won.png';
 import Button from '../components/Button';
 import Content from '../components/Content';
 import MiniCalendar from '../components/MiniCalendar';
-// import TutorItem from '../components/TutorItem';
+import TutorItem from '../components/TutorItem';
+import TutorModal from '../components/TutorModal';
+import './StudentHome.css';
 
-const StudentHome = () => {
+const Student = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [tutors, setTutors] = useState([]);
   const [students, setStudents] = useState([]);
-  const [totalFee, setTotalFee] = useState(0);
   const [currentWeek, setCurrentWeek] = useState(new Date());
   const [today, setToday] = useState({ formattedDate: '', formattedDay: '' });
 
@@ -263,27 +39,7 @@ const StudentHome = () => {
   }, []);
 
   const addTutor = (tutor) => {
-    const newSchedule = tutort.schedule.reduce((acc, sch) => {
-      const key = sch.time;
-      if (acc[key]) {
-        acc[key].push(sch.day);
-      } else {
-        acc[key] = [sch.day];
-      }
-      return acc;
-    }, {});
-
-    const formattedSchedule = Object.entries(newSchedule).map(
-      ([time, days]) => `${days.join(', ')} ${time}`
-    );
-
-    const updatedTutor = {
-      ...tutor,
-      formattedSchedule,
-    };
-
-    setTutors([...tutors, updatedTutor]);
-    setTotalFee(totalFee + tutor.reward);
+    setTutors([...tutors, tutor]);
   };
 
   const calculateTopPosition = (time) => {
@@ -292,7 +48,6 @@ const StudentHome = () => {
     const minuteOffset = (minute / 60) * 20;
     return hourOffset + minuteOffset;
   };
-
   const changeWeek = (direction) => {
     const newDate = new Date(currentWeek);
     newDate.setDate(currentWeek.getDate() + direction * 7);
@@ -303,7 +58,6 @@ const StudentHome = () => {
     const firstDay = new Date(date.getFullYear(), date.getMonth(), 1).getDay();
     return Math.ceil((date.getDate() + firstDay) / 7);
   };
-
   return (
     <>
       <MiniCalendar />
@@ -333,21 +87,8 @@ const StudentHome = () => {
       </div>
       <div className="box-container">
         <Box text={'이번주 진도'} />
-        <div className="week-progress"></div>
+        <Box text={'이번주 과제'} />
       </div>
-
-      <div className="box-container">
-        <div className="week-homework">
-          <Box text={'이번주 숙제 '} />
-        </div>
-      </div>
-      {/* <div className="box-container">
-        <Box text={'이달의 수입'} />
-        <div className="total-fee">
-          <img src={won} alt="won" className="won" />
-          <div>{totalFee.toLocaleString()}원</div>
-        </div>
-      </div> */}
 
       <div className="calendar-container">
         <div className="calendar">
@@ -404,33 +145,26 @@ const StudentHome = () => {
             );
           })}
         </div>
-        <div className="StudentList">
-          <Box text={'과외 목록 | 상점 '} type={'gray'} />
-          {students.map((Tutor, index) => (
+
+        <div className="TutorList">
+          <Box text={'과외 목록 | 상점'} type={'gray'} />
+          {tutors.map((tutor, index) => (
             <TutorItem
               key={index}
-              tutorsub={tutor.tutorsub}
               tutorname={tutor.tutorname}
               tutorsch={tutor.tutorsch}
+              tutorsub={tutor.tutorsub}
               reward={tutor.reward}
-              // name={student.name}
-              // sch={(student.formattedSchedule || []).join(', ')}
-              // grade={student.grade}
-              // sub={student.subject}
             />
           ))}
-          <div className="ButtonContainer">
-            <Button
-              onClick={() => setIsModalOpen(true)}
-              text={'등록'}
-              style={{
-                backgroundColor: '#6ac665',
-                color: 'white',
-                left: 130,
-              }}
-            />
-          </div>
+          <Button
+            onClick={() => setIsModalOpen(true)}
+            className="add-tutor-button"
+            text={'등록'}
+            style={{ backgroundColor: '#6ac665', color: 'white' }}
+          />
         </div>
+        {/* </div> */}
       </div>
       {isModalOpen && (
         <TutorModal onClose={() => setIsModalOpen(false)} onSave={addTutor} />
@@ -439,4 +173,4 @@ const StudentHome = () => {
   );
 };
 
-export default StudentHome;
+export default Student;
