e57cf3f3d0b33086b0ed58f978d2b78e4aa71df5
romans0826 <ans7092@g.skku.edu>
2024-11-16 17:47:07 +0900

    TutorManager 수정

--- frontend/src/pages/Parents.jsx
+++ frontend/src/pages/Parents.jsx
@@ -1,6 +1,110 @@
-import MainCalendar from '../components/MainCalendar';
+import React, { useState, useEffect } from 'react';
+import Cbutton from '../components/Cbutton';
+import Header from '../components/Header';
+import Box from '../components/Box';
+import rarrow from '../img/rightarrow.png';
+import larrow from '../img/leftarrow.png';
+import logo from '../img/logo.png';
+import Button from '../components/Button';
+import Content from '../components/Content';
+import MiniCalendar from '../components/MiniCalendar';
+import TutorItem from '../components/TutorItem';
+import TutorModal from '../components/TutorModal';
+import './Parents.css';
+import LogoutButton from '../components/LogoutButton';
+
 const Parents = () => {
-  return <MainCalendar />;
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [tutors, setTutors] = useState([]);
+  const [students, setStudents] = useState([]);
+  const [currentWeek, setCurrentWeek] = useState(new Date());
+  const [today, setToday] = useState({ formattedDate: '', formattedDay: '' });
+
+  const days = [
+    '월요일',
+    '화요일',
+    '수요일',
+    '목요일',
+    '금요일',
+    '토요일',
+    '일요일',
+  ];
+
+  useEffect(() => {
+    const date = new Date();
+    const dayIndex = date.getDay();
+    const dayName = days[(dayIndex + 6) % 7];
+    const formattedDate = `${date.getDate()}일`;
+    const formattedDay = `(${dayName[0]})`;
+    setToday({ formattedDate, formattedDay });
+  }, []);
+
+  const addTutor = (tutor) => {
+    const newSchedule = tutor.schedule.reduce((acc, sch) => {
+      const key = sch.time;
+      if (acc[key]) {
+        acc[key].push(sch.day);
+      } else {
+        acc[key] = [sch.day];
+      }
+      return acc;
+    }, {});
+
+    const formattedSchedule = Object.entries(newSchedule).map(
+      ([time, days]) => `${days.join(', ')} ${time}`
+    );
+
+    const updatedTutor = {
+      ...tutor,
+      formattedSchedule,
+    };
+
+    setTutors([...tutors, updatedTutor]);
+  };
+
+  const changeWeek = (direction) => {
+    const newDate = new Date(currentWeek);
+    newDate.setDate(currentWeek.getDate() + direction * 7);
+    setCurrentWeek(newDate);
+  };
+
+  const getWeekOfMonth = (date) => {
+    const firstDay = new Date(date.getFullYear(), date.getMonth(), 1).getDay();
+    return Math.ceil((date.getDate() + firstDay) / 7);
+  };
+  return (
+    <>
+      <LogoutButton />
+      <MiniCalendar />
+      <img src={logo} alt="logo" className="logo" />
+
+      <div className="Parents_Progress">
+        <Box text={'수업 진도'} />
+      </div>
+      <div className="WeekAssignment">
+        <Box text={'이번 주 과제'} />
+      </div>
+
+      <div className="calendar-container">
+        <div className="TutorList">
+          <Box text={'과외 목록 '} type={'gray'} />
+          {/* 이부분도 학생 과외 목록에서 불러와야할 것 같아요 */}
+          {/* {tutors.map((tutor, index) => (
+            <TutorItem
+              key={index}
+              tutorname={tutor.tutorname}
+              tutorsch={(tutor.formattedSchedule || []).join(', ')}
+              tutorsub={tutor.tutorsub}
+            />
+          ))} */}
+        </div>
+        {/* </div> */}
+      </div>
+      {isModalOpen && (
+        <TutorModal onClose={() => setIsModalOpen(false)} onSave={addTutor} />
+      )}
+    </>
+  );
 };
 
 export default Parents;
