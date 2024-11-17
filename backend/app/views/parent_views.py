9e28ccb08d7aceb41f27bcca66e1638755896f0d
i-chaeyeon <leechae0605@naver.com>
2024-11-18 00:02:13 +0900

    chore(todo): add todos for upcoming API tasks (parent)
    tutor, student to be updated

--- backend/app/views/parent_views.py
+++ backend/app/views/parent_views.py
@@ -6,3 +6,11 @@ from rest_framework.response import Response
 from rest_framework import status
 import models as table
 from .serializers import UserAccountSerializer 
+
+
+
+## TODO: 과외 목록 Get
+## TODO: 과외 별 수업 진도 get
+## TODO: 수업료 지불 post
+## TODO: 성적 추이 get
+## TODO: 일정 get
