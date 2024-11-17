19650b05ead29e323490cbe4d3cdce5cd89c6913
i-chaeyeon <leechae0605@naver.com>
2024-11-18 00:03:26 +0900

    fix: import

--- backend/app/views/student_views.py
+++ backend/app/views/student_views.py
@@ -4,5 +4,5 @@ from django.shortcuts import redirect
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-import models as table
-from .serializers import UserAccountSerializer 
+from app import models as table
+from app.serializers import UserAccountSerializer 
