622dafbb7e4823c4f431ca8bd3814cd7a27bc414
pea <pea040119@gmail.com>
2024-11-18 10:46:14 +0900

    url 추가

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from app import views
+from app import views 
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -11,5 +11,9 @@ urlpatterns = [
     path('signup/', views.signup_view, name='signup'),  # 회원가입 API
     path('login/', views.login_view, name='login'),      # 로그인 API
     # path("student/", views.student, name='student'),
+    path("tutor/class_register", views.class_register, name='class_register'),
+    path("tutor/class_time_set", views.class_time_set, name='class_time_set'),
+    path("tutor/class_student_set", views.class_student_set, name='class_student_set'),
+    # path("tutor/week_schedule", views.week_schedule, name='week_schedule'),
 ]
  
