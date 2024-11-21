7860acfa143f80d801454c4864102c4cd42c1fba
i-chaeyeon <leechae0605@naver.com>
2024-11-21 12:23:29 +0900

    fix: to run test server

--- backend/app/views/__init__.py
+++ backend/app/views/__init__.py
@@ -1,6 +1,6 @@
 from .account_views import signup_view, login_view
 from .tutor_views import *
-from .utils import *
+from .utils_views import *
 
 # def home(request):
 #     return HttpResponse('홈 페이지')
