0067f5ebb93337c9c61b1016e9fc3dc9c7d9f482
romans0826 <ans7092@g.skku.edu>
2024-11-22 09:04:56 +0900

    tutor 변경

--- frontend/src/pages/Tutor.jsx
+++ frontend/src/pages/Tutor.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom'; // useNavigate 추가
 import { useUser } from '../context/UserContext';
-import axios from 'axios';
 import Cbutton from '../components/Cbutton';
 import './Tutor.css';
 import Header from '../components/Header';
@@ -18,13 +17,18 @@ import LogoutButton from '../components/LogoutButton';
 import ExCalendar from '../components/Excalendar';
 import WeekPopup from '../components/WeekPopup';
 const Tutor = () => {
-  const { user } = useUser();
-  const [tutorId, setTutorId] = useState(null);
+  const { user } = useUser(); // 로그인된 유저 정보 가져오기
+
+  if (user) {
+    console.log('로그인된 유저:', user);
+  } else {
+    console.log('로그인된 유저가 없습니다.');
+  }
+
+  const tutorId = user?.user_id; // 유저에서 tutorId (user_id) 가져오기
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const [students, setStudents] = useState([]);
   const [totalFee, setTotalFee] = useState(0);
-<<<<<<< Updated upstream
-  const [loading, setLoading] = useState(true);  // 로딩 상태
-=======
   const [currentWeek, setCurrentWeek] = useState(new Date());
   const [today, setToday] = useState({ formattedDate: '', formattedDay: '' });
   //추가
@@ -32,31 +36,14 @@ const Tutor = () => {
   const [isWeekPopupOpen, setIsWeekPopupOpen] = useState(false);
 
   const navigate = useNavigate();
->>>>>>> Stashed changes
 
   useEffect(() => {
     if (user) {
-      console.log('로그인된 유저:', user);
-      setTutorId(user.user_id);
-    } else {
-      console.log('로그인된 유저가 없습니다.');
+      const tutorId = user.user_id;
+      console.log('로그인된 tutorId:', user);
     }
   }, [user]);
 
-<<<<<<< Updated upstream
-  useEffect(() => {
-    if (tutorId) {
-      loadStudents(); // tutorId가 설정되면 학생 목록 로드
-    }
-  }, [tutorId]); // tutorId가 변경될 때마다 실행
-
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [currentWeek, setCurrentWeek] = useState(new Date());
-  const [today, setToday] = useState({ formattedDate: '', formattedDay: '' });
-  const navigate = useNavigate();
-
-=======
->>>>>>> Stashed changes
   const days = [
     '월요일',
     '화요일',
@@ -67,39 +54,7 @@ const Tutor = () => {
     '일요일',
   ];
 
-  const loadStudents = async () => {
-    try {
-      console.log('튜터 ID:', tutorId);
-      const response = await axios.get(`http://127.0.0.1:8000/tutor/student_list/${tutorId}/`);
-
-      console.log(response.data);
-      console.log(response.data.classes);
-
-
-      setStudents(response.data.classes);
-
-      //   // 수업료 합산
-      //   const total = response.data.classes.reduce((acc, student) => acc + student.tuition, 0);
-      // setTotalFee(total);
-
-    } catch (error) {
-      console.error('학생 목록 로딩 실패:', error);
-    } finally {
-      setLoading(false);  // 데이터 로딩 완료
-    }
-  };
-
   useEffect(() => {
-    if (students.length > 0) {
-      console.log("students:", students);  // students 값이 업데이트된 후 실행
-      const total = students.reduce((acc, student) => acc + student.tuition, 0);
-      setTotalFee(total);
-    }
-  }, [students]); // students 값이 변경될 때마다 실행
-
-
-  useEffect(() => {
-
     const date = new Date();
     const dayIndex = date.getDay();
     const dayName = days[(dayIndex + 6) % 7];
@@ -108,29 +63,29 @@ const Tutor = () => {
     setToday({ formattedDate, formattedDay });
   }, []);
 
-  // const addStudent = (student) => {
-  //   const newSchedule = student.schedule.reduce((acc, sch) => {
-  //     const key = sch.time;
-  //     if (acc[key]) {
-  //       acc[key].push(sch.day);
-  //     } else {
-  //       acc[key] = [sch.day];
-  //     }
-  //     return acc;
-  //   }, {});
+  const addStudent = (student) => {
+    const newSchedule = student.schedule.reduce((acc, sch) => {
+      const key = sch.time;
+      if (acc[key]) {
+        acc[key].push(sch.day);
+      } else {
+        acc[key] = [sch.day];
+      }
+      return acc;
+    }, {});
 
-  // const formatSchedule = (scheduledClasses) => {
-  //   return scheduledClasses.map(({ day, time }) => `${day} ${time}`).join(', ');
-  // };
+    const formattedSchedule = Object.entries(newSchedule).map(
+      ([time, days]) => `${days.join(', ')} ${time}`
+    );
 
-  //   const updatedStudent = {
-  //     ...student,
-  //     formattedSchedule,
-  //   };
+    const updatedStudent = {
+      ...student,
+      formattedSchedule,
+    };
 
-  //   setStudents([...students, updatedStudent]);
-  //   setTotalFee(totalFee + student.fee);
-  // };
+    setStudents([...students, updatedStudent]);
+    setTotalFee(totalFee + student.fee);
+  };
 
   const calculateTopPosition = (time) => {
     const [hour, minute] = time.split(':').map(Number);
@@ -174,8 +129,8 @@ const Tutor = () => {
               style={{
                 backgroundColor: '#94D2E4',
                 color: 'white',
-                width: '90px',
-                fontSize: '15px',
+                width: '90px', // 원하는 너비 설정
+                fontSize: '15px', // 텍스트 크기 조정
               }}
             />
           </div>
@@ -185,29 +140,16 @@ const Tutor = () => {
           </div>
 
           <div className="StudentList">
-            {loading ? (
-              <p>학생 목록을 불러오는 중입니다...</p>
-            ) : students.length > 0 ? (
-              students.map((student, index) => {
-                const formattedSchedule = student.scheduled_classes
-                  .map(({ day, time }) => `${day} ${time}`)
-                  .join(', ');
-
-                return (
-                  <StudentItem
-                    key={index}
-                    name={student.student_name}
-                    sch={formattedSchedule}
-                    grade={student.grade}
-                    sub={student.subject}
-                  />
-                );
-              })
-            ) : (
-              <p>학생 목록이 없습니다.</p>
-            )}
+            {students.map((student, index) => (
+              <StudentItem
+                key={index}
+                name={student.name}
+                sch={(student.formattedSchedule || []).join(', ')}
+                grade={student.grade}
+                sub={student.subject}
+              />
+            ))}
           </div>
-
         </div>
         <div className="right-section">
           <div className="header-container">
@@ -325,36 +267,6 @@ const Tutor = () => {
                       className="weekday-content"
                       style={{ position: 'relative', marginTop: '10px' }}
                     >
-<<<<<<< Updated upstream
-                      {students.length > 0 ? (
-                        students.flatMap((student, studentIndex) =>
-                          student.scheduled_classes
-                            .filter((sch) => sch.day === day[0])
-                            .map((sch) => (
-                              <div
-                                key={`${student.student_name}-${sch.time}`}
-                                style={{
-                                  position: 'absolute',
-                                  top: `${calculateTopPosition(sch.time)}px`,
-                                }}
-                              >
-                                <Content
-                                  time={sch.time}
-                                  name={`${student.student_name} ${student.subject}`}
-                                  type={ // canceled, makeup 추가해야됨
-                                    today?.formattedDate === formattedDate
-                                      ? 'today' 
-                                      : 'default' 
-                                  }
-                                  onMove={() => { }}
-                                />
-
-                              </div>
-                            ))
-                        )
-                      ) : (
-                        <p> </p>
-=======
                       {students.flatMap((student, studentIndex) =>
                         student.schedule
                           .filter((sch) => sch.day === day[0])
@@ -374,9 +286,7 @@ const Tutor = () => {
                               />
                             </div>
                           ))
->>>>>>> Stashed changes
                       )}
-
                     </div>
                   </div>
                 );
@@ -395,11 +305,9 @@ const Tutor = () => {
 
         {isModalOpen && (
           <StudentModal
-            onClose={() => {
-              loadStudents();
-              setIsModalOpen(false);
-            }}
-            tutorId={tutorId}
+            onClose={() => setIsModalOpen(false)}
+            onSave={addStudent}
+            tutorId={tutorId} // tutorId 전달
           />
         )}
         {isWeekPopupOpen && (
